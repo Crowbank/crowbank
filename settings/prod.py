349d1b348efb8993793db9fc31f4005446780d1a
Eran Yehudai <crowbank.partners@gmail.com>
2018-09-26 17:56:09 +0100

    Corrected path to LOG_FILE

--- settings/prod.py
+++ settings/prod.py
@@ -3,7 +3,7 @@ import sys
 if sys.platform == 'win32':
     LOG_FILE = 'C:\Python27\Lib\Site-packages\petadmin\logs\crowbank.log'
 else:
-    LOG_FILE = '/usr/lib/site-packages/crowbank/logs/crowbank.log'
+    LOG_FILE = '/usr/lib/python2.7/site-packages/crowbank/logs/crowbank.log'
 
 DB_SERVER = 'HP-SERVER\\SQLEXPRESS'
 DB_USER = 'PA'
