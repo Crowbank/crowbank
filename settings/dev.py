579580a4bea0ebd2970371414846b8890d5a31cd
Eran Yehudai <crowbank.partners@gmail.com>
2018-09-26 17:40:33 +0100

    Added LOG_FILE settings under cygwin

--- settings/dev.py
+++ settings/dev.py
@@ -1,4 +1,9 @@
-LOG_FILE = 'C:\Python27\Lib\Site-packages\petadmin\logs\qa.log'
+import sys
+
+if sys.platform == 'win32':
+    LOG_FILE = 'C:\Python27\Lib\Site-packages\crowbank\logs\qa.log'
+else:
+    LOG_FILE = '/usr/lib/site-packages/crowbank/logs/qa.log'
 
 DB_SERVER = 'HP-SERVER\\SQLEXPRESS_dev'
 DB_USER = 'PA'
--- settings/prod.py
+++ settings/prod.py
@@ -1,4 +1,9 @@
-LOG_FILE = 'C:\Python27\Lib\Site-packages\petadmin\logs\crowbank.log'
+import sys
+
+if sys.platform == 'win32':
+    LOG_FILE = 'C:\Python27\Lib\Site-packages\petadmin\logs\crowbank.log'
+else:
+    LOG_FILE = '/usr/lib/site-packages/crowbank/logs/crowbank.log'
 
 DB_SERVER = 'HP-SERVER\\SQLEXPRESS'
 DB_USER = 'PA'
