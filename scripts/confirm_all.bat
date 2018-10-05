da18a84d20fb823745363eb07d8440aa1fbb29f2
Eran Yehudai <crowbank.partners@gmail.com>
2018-10-05 12:21:02 +0100

    Various adaptations for cygwin

---
+++ scripts/confirm_all.bat
@@ -0,0 +1,1 @@
+C:\cygwin64\bin\bash.exe --login /usr/lib/python2.7/site-packages/crowbank/scripts/confirm_all
--- settings/prod.py
+++ settings/prod.py
@@ -1,7 +1,7 @@
 import sys
 
 if sys.platform == 'win32':
-    LOG_FILE = 'C:\Python27\Lib\Site-packages\petadmin\logs\crowbank.log'
+    LOG_FILE = 'C:\Python27\Lib\Site-packages\crowbank\logs\crowbank.log'
 else:
     LOG_FILE = '/usr/lib/python2.7/site-packages/crowbank/logs/crowbank.log'
 
