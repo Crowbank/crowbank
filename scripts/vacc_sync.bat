19484ee1ce62ab08b834fccd96754dd4e4df3fb2
Eran Yehudai <crowbank.partners@gmail.com>
2018-10-10 13:00:52 +0100

    Added vacc_sync.bat

---
+++ scripts/vacc_sync.bat
@@ -0,0 +1,1 @@
+winscp.com /command "open -privatekey=c:\users\the_y\.ssh\putty_key.ppk crowbank@cp165172.hpdns.net" "synchronize both d:\dropbox\kennels\Vaccinations /home/crowbank/dev/vaccinations" close exit
