b0d6b46a2638fbd5df3cf14e8d24a97c6fb924ac
Eran Yehudai <crowbank.partners@gmail.com>
2018-10-26 11:26:14 +0100

    Changed img folder location to python3.7 folder

--- settings/base.py
+++ settings/base.py
@@ -13,11 +13,11 @@ EMAIL_LOGS = 'crowbank.partners@gmail.com'
 EMAIL_REPLYTO = 'info@crowbank.co.uk'
 
 if sys.platform == 'win32':
-    IMAGE_FOLDER = 'C:\Python27\Lib\Site-packages\crowbank\img'
+    IMAGE_FOLDER = 'C:\Program Files\Python37\Lib\Site-packages\crowbank\img'
     CONFIRMATIONS_FOLDER = 'D:\Dropbox\Kennels\Confirmations'
     VACC_FOLDER = 'K:\Vaccinations'
 else:
-    IMAGE_FOLDER = '/usr/lib/python2.7/site-packages/crowbank/img'
+    IMAGE_FOLDER = '/usr/lib/python3.7/site-packages/crowbank/img'
     CONFIRMATIONS_FOLDER = '/dropbox/Kennels/Confirmations'
     VACC_FOLDER = '/dropbox/Kennels/Vaccinations'
 CROWBANK_ADDRESSES = ['info@crowbank.co.uk', 'crowbank.partners@gmail.com', 'eyehudai@gmail.com']
