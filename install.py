280d482370843ce96c8db2d083a53da4ab723eee
Eran Yehudai <crowbank.partners@gmail.com>
2019-02-13 13:14:56 +0000

    Encapsulate package installation in a simple function

--- install.py
+++ install.py
@@ -3,20 +3,25 @@ from subprocess import run
 import os.path
 from site import getsitepackages
 
+def install_package(package_name):
+    package_dir = getsitepackages()[-1]
+    package_path = os.path.join(package_dir, package_name)
+
+    if os.path.exists(package_path):
+        shutil.rmtree(package_path)
+
+    shutil.copytree(package_name, package_path)
+
+
 PIP_ARGS = ['pip', 'install', '-r', 'requirements.txt']
 SCRIPT_DIR = r'C:\\Crowbank'
+FILE_LIST = ('confirm.py', 'scan_vaccinations.py', 'sms.py', 'wsync.sh')
+PACKAGE_LIST = ('pypa', 'cloud')
 
-package_dir = getsitepackages()[-1]
-pypa_path = os.path.join(package_dir, 'pypa')
+for package in PACKAGE_LIST:
+    install_package(package)
 
-if os.path.exists(pypa_path):
-    shutil.rmtree(pypa_path)
-
-shutil.copytree('pypa', pypa_path)
-
-files = ('confirm.py', 'scan_vaccinations.py', 'sms.py', 'wsync.sh')
-
-for file in files:
+for file in FILE_LIST:
     shutil.copy(file, SCRIPT_DIR)
 
 run(PIP_ARGS)
