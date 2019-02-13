175bdf6ecfcad40dc8a6b25592fc6012ebb59e3e
Eran Yehudai <crowbank.partners@gmail.com>
2019-02-13 13:14:19 +0000

    Added functionality to copy packages and install requirements

--- install.py
+++ install.py
@@ -1,8 +1,18 @@
 import shutil
 from subprocess import run
+import os.path
+from site import getsitepackages
 
 PIP_ARGS = ['pip', 'install', '-r', 'requirements.txt']
 SCRIPT_DIR = r'C:\\Crowbank'
+
+package_dir = getsitepackages()[-1]
+pypa_path = os.path.join(package_dir, 'pypa')
+
+if os.path.exists(pypa_path):
+    shutil.rmtree(pypa_path)
+
+shutil.copytree('pypa', pypa_path)
 
 files = ('confirm.py', 'scan_vaccinations.py', 'sms.py', 'wsync.sh')
 
@@ -10,3 +20,4 @@ for file in files:
     shutil.copy(file, SCRIPT_DIR)
 
 run(PIP_ARGS)
+
